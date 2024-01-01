9d5c9274eb3c74a2c9d30e313466e288ed33acc8
taofikarianto <taofikarianto@gmail.com>
2024-01-02 00:55:54 +0700

    feat: service

---
+++ src/models/service.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { v4: uuidv4 } = require("uuid");
+
+const Service = new Schema(
+    {
+        _id: {
+            type: String,
+            default: uuidv4,
+        },
+        name: {
+            type: String,
+            required: false,
+        },
+        typeservice: {
+            type: String,
+            required: false,
+        },
+        detailService: {
+            type: String,
+            required: false,
+        },
+        image: {
+            type: String,
+            required: false,
+        },
+        note: {
+            type: String,
+            required: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("Service", Service);
